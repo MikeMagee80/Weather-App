6b3aa069fc1f8dad01192c059a5e6f4f1978f95e
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-04-05 09:32:33 -0500

    Search function results fixed.
    Fixed typo in API call that prevented it from returning data in metric measurements.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
@@ -10,8 +11,21 @@ search.addEventListener('click', () => {
 
     if (city == '')
         return;
+// to change unit types, alter next line element to units=imperial, units=metric, standard is default
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&unites=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        if (json.cod == '404') {
+            container.style.height = '400px';
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            error404.classList.add('active');
+            return;
+        }
+
+        container.style.height = '555px';
+        weatherBox.classList.add('active');
+        weatherDetails.classList.add('active');
+        error404.classList.remove('active');
 
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
@@ -36,11 +50,22 @@ search.addEventListener('click', () => {
                 image.src = 'images/cloud.png';
                 break;
             
-            
+            case 'Mist':
+                image.src = 'images/mist.png';                
+                break;
+
+            case 'Haze':
+                image.src = 'images/mist.png';                
+                break;
                 
             default:
-                break;
+                image.src = 'images/cloud.png';
         }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
     });
 
